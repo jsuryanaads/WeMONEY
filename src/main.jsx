3cb3451c47073c0c85441af88e39a0b77850cf43
jsuryanaads <jsuryanaads@gmail.com>
2026-09-08 22:58:58 +0700

    feat: load centralized We MONEY visual layer

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 import { AuthProvider } from './hooks/useAuth'
 import './styles/index.css'
+import './styles/wemoney-v2.css'
 import { applyTheme, getInitialTheme } from './hooks/useTheme'
 
 applyTheme(getInitialTheme())
