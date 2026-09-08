a40d79eda468ce333dc74bb0783acfddd6b7dee3
Jajang Suryana <jsuryanaads@gmail.com>
2026-09-09 06:18:47 +0700

    feat: use master PNG for app favicon

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,17 @@ import './styles/wemoney-v2.css'
 import './styles/accessibility.css'
 import './styles/capture-menu.css'
 import { applyTheme, getInitialTheme } from './hooks/useTheme'
+import appIcon from './assets/branding/wemoney-app-icon.png'
 
 applyTheme(getInitialTheme())
+
+const favicon = document.querySelector('link[rel="icon"]')
+if (favicon) {
+  favicon.type = 'image/png'
+  favicon.href = appIcon
+}
+
+document.querySelector('link[rel="manifest"]')?.remove()
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
