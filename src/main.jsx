8ee691140c660f5021e8e461a51a3169fe856979
jsuryanaads <jsuryanaads@gmail.com>
2026-09-08 23:50:44 +0700

    chore: load accessibility polish layer

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './app/App'
 import { AuthProvider } from './hooks/useAuth'
 import './styles/index.css'
 import './styles/wemoney-v2.css'
+import './styles/accessibility.css'
 import { applyTheme, getInitialTheme } from './hooks/useTheme'
 
 applyTheme(getInitialTheme())
